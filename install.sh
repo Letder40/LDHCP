5d2d62ac04ba10a416c2e1895c950bbdacfc7360
Letder40 <letder40@gmail.com>
2023-12-29 21:07:39 +0100

    created build script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function make_ldhcp() {
+   if [[ ! -e "./build" ]]; then
+      mkdir build
+      echo "[#] Created build directory"
+      cd build
+      cmake .. &>/dev/null
+      echo "[#] LDHCP successfully compiled"
+      echo "[#] binary: $(pwd)/ldhcp" 
+      cd ..
+   else
+      cd build
+      make .
+      echo "[#] LDHCP successfully compiled"
+      echo "[#] binary: $(pwd)/ldhcp" 
+      cd ..
+   fi
+}
+
+make_ldhcp
